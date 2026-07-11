32735d27d36acdbbdb21032a42f5f7e7f3097ffb
wail735 <chennoufwail@gmail.com>
2026-07-11 22:22:20 +0100

    fix: update contact page map location to Blida

--- frontend/src/components/Contact/MapSection.jsx
+++ frontend/src/components/Contact/MapSection.jsx
@@ -4,8 +4,8 @@ function MapSection() {
   return (
     <section className="w-full h-[500px] relative">
       <iframe
-        title="Google Maps Boston"
-        src="https://maps.google.com/maps?q=Boston,MA&t=&z=13&ie=UTF8&iwloc=&output=embed"
+        title="Google Maps Blida"
+        src="https://maps.google.com/maps?q=Blida,Algeria&t=&z=13&ie=UTF8&iwloc=&output=embed"
         className="absolute inset-0 w-full h-full border-0"
         allowFullScreen=""
         loading="lazy"
